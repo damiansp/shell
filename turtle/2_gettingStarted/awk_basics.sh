828ad24034e4e0bbc31306745910b84892b4a9fb
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-02 13:35:00 -0400

    awk++

--- turtle/2_gettingStarted/awk_basics.sh
+++ turtle/2_gettingStarted/awk_basics.sh
@@ -11,4 +11,17 @@ head -10 ../alice.txt | awk '{ print $1, $NF }'
 
 echo "Print non-empty lines:"
 head -10 ../alice.txt | awk 'NF > 0 { print $0 }'
-head -10 ../alice.txt | awk 'NF > 0'  # same (print is default)
+head -10 ../alice.txt | awk 'NF < 1 { print "EMPTY" }' 
+
+
+# -F to change field delimiter
+head -10 ../alice.txt | awk -Fe '{ print $1 }'
+
+# -v for output field separator
+head -10 ../alice.txt | awk -Fe -v 'OFS=... ' '{ print $1, $3}'
+
+# Insert text
+head -10 ../alice.txt | awk -Fa '{ print "And: " $1 }'
+
+# Format strings
+head -10 ../alice.txt | awk  '{ printf "First: %s, and last: %s\n", $1, $NF }'
