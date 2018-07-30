858caaacaef8c62cc95c0085d5d052d931ab8927
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-07-30 14:14:30 -0700

    fin password to dir

--- turtle/5_pipelines/password_to_dir.sh
+++ turtle/5_pipelines/password_to_dir.sh
@@ -8,3 +8,24 @@
 #   ypcat passwd | passwd-to-directory > office-directory-file
 #   niscat passwd.org_dir | passwd-to-directory > office-directory-file
 
+#unmask 077                  # restrict tmp files to just me (not on OSX)
+PERSON=/tmp/pd.key.person.$$ # $$ is the shell variable
+OFFICE=/tmp/pd.key.office.$$
+PHONE=/tmp/pd.key.phone.$$
+USER=/tmp/pd.key.user.$$
+
+trap "exit 1"
+trap "rm -f $PERSON $OFFICE $PHONE $USER" # delete tmp files even on failure
+
+awk -F: '{ print $1 ":" $5 }' > $USER # read fields 1, 5 from stdin
+sed -e 's=/.*=  =' \
+    -e 's=^\([^:]*\):\(.*\) \([^ ]*\)=\1:\3, \2=' < $USER | sort > $PERSON
+sed -e 's=^\([^:]*\):[^/]*/\([^/]*\)/.*$=\1:\2=' < $USER | sort > $OFFICE
+sed -e 's=^\([^:]*\):[^/]*/\([^/]*\)=\1:\2=' < $USER | sort > $PHONE
+
+join -t: $PERSON $OFFICE |
+    join -t: - $PHONE |
+    cut -d: -f 2- |                                     # keep field 2 - end
+    sort -t: -k1,1 -k2,2 -k3,3 |                        # sort by last name
+    awk -F: '{ printf("%-39s\t%s\t%s\n", $1, $2, $3) }' # format
+
