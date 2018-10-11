afa89229c65ccf66304002cadc5a30576cbfd66b
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-10-11 00:58:49 -0400

    +printflags

---
+++ turtle/7_ioFilesCommandEval/printflags.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+printf "|%-10s| |%10s|\n" hello world
+printf "|% d| |% d|\n" 15 -15
+printf "%+d %+d\n" 15 -15
+printf "%x %#x\n" 15 15
+printf "%-5d\n" 15
+printf "%s is %d\n" D "'D"
